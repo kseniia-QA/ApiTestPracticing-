579f97c6aac5302b79932a76e8fa984d89188d23
kseniia <kshamtova22@mail.ru>
2021-11-01 10:25:47 +0300

    Get response tests

---
+++ src/test/java/org/web/test/api/TestStudentsData.java
@@ -0,0 +1,28 @@
+package org.web.test.api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class TestStudentsData {
+    String getStudentEndpoint = "https://reqres.in/api/users?page=2";
+
+    @Test
+    public void validateStudentData() {
+
+Response response =	RestAssured.get(getStudentEndpoint);
+System.out.println(response.getStatusCode());
+System.out.println("All headers value" + response.getHeaders());
+
+//fetch specific header value/
+        System.out.println(response.getHeader("Age"));
+
+//fetch body data//
+
+System.out.println(response.asString());
+
+System.out.println(response.asPrettyString());
+
+    }
+
+}
